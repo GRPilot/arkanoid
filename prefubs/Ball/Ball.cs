2ce5bc60784d929dcfa624b2cf5371d14093e5ed
GRPilot <rusan.rusik@yandex.ru>
2021-07-05 07:44:53 +0300

    init ball script

---
+++ prefubs/Ball/Ball.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class Ball : RigidBody2D {
+    public override void _Ready() {
+    }
+
+    public override void _PhysicsProcess(float delta) {
+        base._PhysicsProcess(delta);
+    }
+}
